5d7c6532654d1f4d1facdfbcfe1b1bce5ef27f13
Alan <sinderpl@gmail.com>
2017-03-30 10:36:39 +0100

    Get leaderboard ready for testing"

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.post('/quotes', (req, res) => {
 // http://localhost:3000/user/Alan%20Niemiec/2345
 app.post('/user', getUserData);
 
-app.get('/leaderboard');
+app.get('/leaderboard' ,getLeaderboard);
 
-app.get('/addRecord');
+app.get('/addRecord', addRecord);
     //MONGO code
 //Mongo instance
 var db;
@@ -43,7 +43,7 @@ var db;
 var currentUser;
 var currentUserAccounts = new Map();
 
-MongoClient.connect("mongodb://Test:Test@ds139187.mlab.com:39187/heroku_vh3f7203", (err, database) => {
+MongoClient.connect("mongodb://testBasketball:password12345@ds145220.mlab.com:45220/heroku_sjst9tqr", (err, database) => {
   db = database;
   console.log("db connection ready");
 
@@ -52,3 +52,42 @@ MongoClient.connect("mongodb://Test:Test@ds139187.mlab.com:39187/heroku_vh3f7203
     console.log("App now running on port", port);
   });
 })
+
+
+function findOne(collectionName, query, callback){
+   db.collection(collectionName).findOne(query, function (err, item){
+     if (err){
+       callback(err);
+     }
+     else{
+       //Return the data with no errors
+       callback(null, item);
+    }
+   });
+ }
+
+function getLeaderboard(req, res){
+  //Constuct the query
+  var query = {"users"};
+  //Find one user only in the database
+  findOne("leaderboard", query, function (err, item){
+      //Log any errors
+      if(err) {
+       console.log(err);
+        return;
+     }
+
+      //If result is not empty return the data
+      if(item){
+        console.log(item);
+        console.log("Leaderboard has been found");
+         res.type('json');
+         res.json(item);
+
+      }
+      else{
+        console.log("Leaderboard not found\n");
+        res.status(500).send("Leaderboard has not been found");
+    }
+  });
+}
